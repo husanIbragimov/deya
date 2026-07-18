c4bda77ad9289ee6b819dae1d4ca765cbf249b66
Husan Ibragimov <husandev@mail.ru>
2026-07-18 11:56:09 +0500

    Update Docker commands in build and down scripts to use 'docker compose' syntax

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 echo "Building the project..."
 git pull origin main && echo "Project updated from the repository."
-cd docker/ && docker-compose --env-file ./../.envs/.env up --build -d
+cd docker/ && docker compose --env-file ./../.envs/.env up --build -d
 echo "Remove none images from computer"
 docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
 echo "Containers running:"
